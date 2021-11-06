fb47da525fe933279d64b7a3398584b255275963
e1b19082 <itpassport.kh@gmail.com>
2021-11-07 05:50:41 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/z0209/kaizi/janken2/Janken2ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z0209.kaizi.janken2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janken2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
